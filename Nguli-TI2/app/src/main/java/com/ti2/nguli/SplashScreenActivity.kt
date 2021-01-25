29a3c9a46c6898e1bcf06cba87ea359901877e33
Irfan Rizqulloh <18102269@ittelkom-pwt.ac.id>
2021-01-26 02:54:32 +0700

    Sprint-3 SplashScreenActivity Function

--- Nguli-TI2/app/src/main/java/com/ti2/nguli/SplashScreenActivity.kt
+++ Nguli-TI2/app/src/main/java/com/ti2/nguli/SplashScreenActivity.kt
@@ -3,13 +3,19 @@ package com.ti2.nguli
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-        finish()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+
     }
 }
