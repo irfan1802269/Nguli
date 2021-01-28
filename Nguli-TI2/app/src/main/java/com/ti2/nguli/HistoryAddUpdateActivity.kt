1dde823fe20afa24277284a8e4813191da95e364
Irfan Rizqulloh <18102269@ittelkom-pwt.ac.id>
2021-01-29 03:13:11 +0700

    Sprint-3 HistoryAddUpdateActivity

---
+++ Nguli-TI2/app/src/main/java/com/ti2/nguli/HistoryAddUpdateActivity.kt
@@ -0,0 +1,11 @@
+package com.ti2.nguli
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HistoryAddUpdateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history_add_update)
+    }
+}
