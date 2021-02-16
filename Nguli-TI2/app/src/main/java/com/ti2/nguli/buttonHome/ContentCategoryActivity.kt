22218d7c12a9913e977f60e445c14462e42e73e8
Irfan Rizqulloh <18102269@ittelkom-pwt.ac.id>
2021-02-16 23:51:21 +0700

    Sprint-4 ContentCategoryActivity

---
+++ Nguli-TI2/app/src/main/java/com/ti2/nguli/buttonHome/ContentCategoryActivity.kt
@@ -0,0 +1,12 @@
+package com.ti2.nguli.buttonHome
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ti2.nguli.R
+
+class ContentCategoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_content_category)
+    }
+}
