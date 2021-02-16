daaf8c2d850a8beb49ff1efa2170c5b9f224e6e6
Irfan Rizqulloh <18102269@ittelkom-pwt.ac.id>
2021-02-16 23:41:46 +0700

    Sprint-4 ContentBundleActivity

---
+++ Nguli-TI2/app/src/main/java/com/ti2/nguli/buttonHome/ContentBundleActivity.kt
@@ -0,0 +1,12 @@
+package com.ti2.nguli.buttonHome
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ti2.nguli.R
+
+class ContentBundleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_content_bundle)
+    }
+}
